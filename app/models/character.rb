0f3ad1d695cb19f013aea9783fae46f8e5a070e1
mcoduti <matthew.coduti@gmail.com>
2015-08-24 00:15:01 -0500

    added js to dashboard

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,10 @@
 class Character < ActiveRecord::Base
+  validates :language, :presence => true #:uniqueness => true
+  validates :translation, :presence => true #:uniqueness => true
+  validates :tone, :presence => true #:uniqueness => true
+  validates :english_spelling, :presence => true #:uniqueness => true
+  validates :character, :presence => true #:uniqueness => true
+
+
+
 end
