a5ff28b2d3dd9216c6017189f33ad9fa77781c25
mcoduti <matthew.coduti@gmail.com>
2015-08-26 21:11:24 -0500

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Character.create(language: 'mandarin', translation: 'I', tone: '3rd', english_spelling: 'wǒ', character: '我')
+Character.create(language: 'mandarin', translation: 'you', tone: '3rd', english_spelling: 'nǐ', character: '你')
+Character.create(language: 'mandarin', translation: 'am/are/is', tone: '4th', english_spelling: 'shì', character: '是')
+Character.create(language: 'mandarin', translation: 'I + de = mine', tone: '5th', english_spelling: 'de', character: '的')
+Character.create(language: 'mandarin', translation: 'little', tone: '3rd', english_spelling: 'xiǎo', character: '小')
